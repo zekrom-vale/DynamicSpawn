5ce1ee257e5e9c4dcc63ac043039cee6cac965cf
zekrom-vale <sgraven66@gmail.com>
2018-03-31 15:52:12 -0500

    Updated reset

--- DynamicSpawn/DynamicSpawn/reset.ps1
+++ DynamicSpawn/DynamicSpawn/reset.ps1
@@ -1,19 +1,18 @@
 Write-Output "Running"
 $item = Get-ChildItem . *.json.patch -rec
 $prev= Get-Content prevVal.txt
-$val= "    `"value`": `"apex, avian, floran, glitch, human, hylotl, novakid`""
-$prev2= Get-Content prevVal2.txt
-$val2= "`"value`": `"apex, avian, human, hylotl`"    "
+$prev -Split '`n'
+$val= "    `"value`":`"apex,avian,floran,glitch,human,hylotl,novakid`""
+$val2= "`"value`":`"apex,avian,human,hylotl`"    "
 
 foreach ($file in $item){
     (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace $prev, $val } |
+    Foreach-Object { $_ -replace $prev[0], $val } |
     Set-Content $file.PSPath
 
     (Get-Content $file.PSPath) |
-    Foreach-Object { $_ -replace $prev2, $val2 } |
+    Foreach-Object { $_ -replace $prev[1], $val2 } |
     Set-Content $file.PSPath
 }
-
+$val+="`n$($val2)"
 $val| Set-Content 'prevVal.txt'
-$val2| Set-Content 'prevVal2.txt'
