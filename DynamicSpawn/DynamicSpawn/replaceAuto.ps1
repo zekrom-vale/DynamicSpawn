bc08ae9b8f172356d16dabfd64bd5c2ddc0f3baa
zekrom-vale <sgraven66@gmail.com>
2018-04-01 21:21:01 -0500

    Rekeyed to //A and //B

--- DynamicSpawn/DynamicSpawn/replaceAuto.ps1
+++ DynamicSpawn/DynamicSpawn/replaceAuto.ps1
@@ -103,8 +103,8 @@ function core($val, $val2){
 	$item = Get-ChildItem . *.json.patch -rec
 	$prev= Get-Content prevVal.txt
 	$prev -Split '`n'
-	$val= "    `"value`":`"$($val)`""
-	$val2= "`"value`":`"$($val2)`"    "
+	$val= "`"value`":`"$($val)`" //A"
+	$val2= "`"value`":`"$($val2)`" //B"
 
 	foreach ($file in $item){
 		(Get-Content $file.PSPath) |
