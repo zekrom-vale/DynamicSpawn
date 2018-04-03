ecf26594f781fd72ea4d1fd072186448f25286d7
zekrom-vale <sgraven66@gmail.com>
2018-04-03 16:29:24 -0500

    Added keys and des and added spaces

--- DynamicSpawn/DynamicSpawn/replaceAuto.ps1
+++ DynamicSpawn/DynamicSpawn/replaceAuto.ps1
@@ -5,39 +5,49 @@ function startFn{
 	$species= Int
 	If($full){
 		Write-Host "Does not include "-ForegroundColor Red -NoNewline
-		Write-Host "vanilla defaults!"-ForegroundColor Green
+		Write-Host "vanilla defaults!"-ForegroundColor Yellow
 	}
 	Else{
-		Write-Host "Includes vanilla defaults automatically" -ForegroundColor Green
+		Write-Host "Includes vanilla defaults automatically" -ForegroundColor Yellow
 	}
+	#===============================
 	$cont=@(
 		@("apex","avian","floran","glitch","human","hylotl","novakid"),
 		@("apex","avian","human","hylotl")
 	)
+	$des=@(
+		"Generic NPC Spawn",
+		"Scientific and cult NPC Spawn"
+	)
+	$key=@(
+		"A",
+		"B"
+	)
+	#===============================
 	$val=@()
 	For($i=0; $i -lt $cont.Length; $i++){
+		Write-Host "Modifies: $($des[$i])" -ForegroundColor Cyan
 		Write-Host "Default: $($cont[$i])" -ForegroundColor Green
-		Write-Host 'Type "end" to end the input' -ForegroundColor Yellow
-		$l=65+$i
+		Write-Host "    Type `"end`" to end the input" -ForegroundColor Yellow
 		If($full){
-			$val+=looping @() $l
+			$val+=looping @() $key[$i]
 		}
 		Else{
-			$val+=looping $cont[$i] $l
+			$val+=looping $cont[$i] $key[$i]
 		}
 	}
-	core $val
+	core $val $key
 }
 
 function Int{
-	$species= Get-Content species.txt
+	$species= Get-Content 'species.txt'
 	$species -Split '`n'
 	return $species
 }
 
 function looping($array, $run){
-	For($n=$array.Length+1; ; $n++){
-		Write-Host "$([char]$run)" -ForegroundColor Green -NoNewline
+	For($n=$array.Length+1;; $n++){
+		Write-Host "$($run)" -ForegroundColor Green -NoNewline
 		$userInput= Read-Host " New Spawn Value $($n)"
 		$userInput= $userInput -replace '\s',''
 		If($userInput -like '$exit'){
@@ -53,20 +63,20 @@ function looping($array, $run){
 		}
 		If(-not($userInput -match '\w+')){
 			$n--
-			Write-Host "ERROR: Invalid input" -ForegroundColor Red
+			Write-Host "`tERROR: Invalid input" -ForegroundColor Red
 			continue
 		}
 		If($array -contains $userInput){
 			$n--
-			Write-Host "ERROR: Duplicate input" -ForegroundColor Red
+			Write-Host "`tERROR: Duplicate input" -ForegroundColor Red
 			continue
 		}
 		If(-not($species -contains $userInput)){
-			Write-Host "ERROR: $($userInput) not found." -ForegroundColor Red
-			Write-Host "S: Saves $($userInput) to species list
-I: Ignores $($userInput)
-R: Removes $($userInput) from change
-F: Forgets all values" -ForegroundColor Yellow
+			Write-Host "    ERROR: $($userInput) not found." -ForegroundColor Red
+			Write-Host "      S: Saves $($userInput) to species list
+      I: Ignores $($userInput)
+      R: Removes $($userInput) from change
+      F: Forgets all values" -ForegroundColor Yellow
 			$do= act $userInput
 			If($do -eq 'R'){
 				$n--
@@ -81,7 +91,7 @@ F: Forgets all values" -ForegroundColor Yellow
 }
 
 function act($userInput){
-	Write-Host 'Action: ' -ForegroundColor Magenta -NoNewline
+	Write-Host "    Action: " -ForegroundColor Magenta -NoNewline
 	$key= Read-Host
 	If($key -like 'S'){
 		Add-Content -Path "species.txt" -Value $userInput
@@ -94,6 +104,7 @@ function act($userInput){
 		return 'R'
 	}
 	ElseIf($key -like 'F'){
+		cls
 		startFn
 		exit
 	}
@@ -101,19 +112,19 @@ function act($userInput){
 		exit
 	}
 	Else{
-		Write-Host 'ERROR: Invalid selection' -ForegroundColor Red
+		Write-Host "      ERROR: Invalid selection" -ForegroundColor Red
 		act $userInput
 	}
 }
 
-function core($valInt){
+function core($valInt, $key){
 	$item = Get-ChildItem . *.json.patch -rec
-	$prev= Get-Content prevVal.txt
+	$prev= Get-Content 'prevVal.txt'
 	$prev -Split '`n'
 	$val= @()
 	For($i=0; $i -lt $valInt.Length; $i++){
 		$l=$i+65
-		$val+= "`"value`":`"$($valInt[$i])`" //$([char]$l)"
+		$val+= "`"value`":`"$($valInt[$i])`" //$($key)"
 	}
 
 	foreach ($file in $item){
@@ -123,9 +134,7 @@ function core($valInt){
 			Set-Content $file.PSPath
 		}
 	}
-	Read-Host '??'
-	$val+="`n$($val2)"
-	$val| Set-Content 'prevVal.txt'
+	$val -join "`n"| Set-Content 'prevVal.txt'
 	exit
 }
 startFn
