895584059596590325b32fbf6cf45fa63de318b9
zekrom-vale <sgraven66@gmail.com>
2018-06-21 18:24:22 -0500

    Now navagates and remembers position for both

--- DynamicSpawn/js/nav.js
+++ DynamicSpawn/js/nav.js
@@ -26,46 +26,50 @@ addEventListener("load",()=>{
 	}
 }
 
-document.getElementById("speciesList").addEventListener("keydown",function(event){
-	//Will not work with caret browsing mode
-	//https://support.mozilla.org/en-US/questions/974774
-	var selected=document.activeElement,
-	key=selected.dataset.key||0,
-	to,li;
-	if(this.contains(selected)){
-		while(selected.tagName!="LI")selected=selected.parentNode;
+document.getElementById("body").addEventListener("keydown",function(event){
+	if(event.shiftKey)move.call(document.querySelector("#npcList>div.active>ul"),event.key);
+	else move.call(document.getElementById("speciesList"),event.key);
+	function move(input){
+		var selected=this.querySelector("[data-active]")||this.querySelector("li:first-of-type"),//document.activeElement,
+		key=selected.dataset.active||selected.dataset.key||0,
+		to,li;
+		switch(input){
+			case "ArrowUp":
+				li=selected;
+				do{
+					if(!li)return;
+					li=li.previousSibling;
+				}while(isValid(li));
+				if(!li)return;
+				to=li.querySelector(`[data-key="${key}"]`);
+				break;
+			case "ArrowDown":
+				li=selected;
+				do{
+					if(!li)return;
+					li=li.nextSibling;
+				}while(isValid(li));
+				if(!li)return;
+				to=li.querySelector(`[data-key="${key}"]`);
+				break;
+			case "ArrowRight":
+				key=(key+1)%3;
+				to=selected.querySelector(`[data-key="${key}"]`);
+				break;
+			case "ArrowLeft":
+				key=(key-1)%3;
+				to=selected.querySelector(`[data-key="${key}"]`);
+				break;
+		}
+		if(to){
+			if(li)delete selected.dataset.active;
+			(li||selected).dataset.active=key;
+			to.focus();
+		}
+		function isValid(li){
+			return !(li instanceof HTMLElement)||li.style.display=="none"||(li.classList.contains("hideMod")&&/^\s*null/.test(document.getElementById("activeStyle2").innerHTML));
+		}
 	}
-	switch(event.key){
-		case "ArrowUp":
-			li=selected;
-			do{
-				li=li.previousSibling;
-				console.log(li);
-			}while(!(li instanceof HTMLElement)||li.style.display=="none");
-			if(!li)return;
-			to=li.querySelector(`[data-key="${key}"]`);
-			break;
-		case "ArrowDown":
-			li=selected;
-			do{
-				li=li.nextSibling;
-			}while(!(li instanceof HTMLElement)||li.style.display=="none");
-			if(!li)return;
-			to=li.querySelector(`[data-key="${key}"]`);
-			break;
-		case "ArrowRight":
-			key=(key+1)%3;
-			to=selected.querySelector(`[data-key="${key}"]`);
-			break;
-		case "ArrowLeft":
-			key=(key-1)%3;
-			to=selected.querySelector(`[data-key="${key}"]`);
-			break;
-	}
-	console.log(to);
-	if(li)delete selected.dataset.active;
-	(li||selected).dataset.active=key;
-	to.focus();
 });
 
 });
