7fad3b5e0b5f8560f62983cc75723b8b9e4b5aaa
zekrom-vale <sgraven66@gmail.com>
2018-06-21 21:09:53 -0500

    Updates to selection and now always navigate -input

--- DynamicSpawn/js/nav.js
+++ DynamicSpawn/js/nav.js
@@ -25,56 +25,65 @@ addEventListener("load",()=>{
 		}
 	}
 }
+});
 
-document.getElementById("body").addEventListener("keydown",function(event){
-	if(event.shiftKey)move.call(document.querySelector("#npcList>div.active>ul"),event.key);
-	else move.call(document.getElementById("speciesList"),event.key);
-	function move(input){
-		if(/input|textarea/i.test(document.activeElement.tagName))
-		var selected=this.querySelector("[data-active]")||this.querySelector("li:first-of-type"),//document.activeElement,
-		key=selected.dataset.active||selected.dataset.key||0,
-		to,li;
-		switch(input){
-			case "ArrowUp":
-			case "W":
-				li=selected;
-				do{
+addEventListener("keydown",function(event){
+	if(!/input|textarea/i.test(document.activeElement.tagName)){
+		if(event.shiftKey)move.call(document.querySelector("#npcList>div.active>ul"),event.key);
+		else move.call(document.getElementById("speciesList"),event.key);
+		function move(input){
+			var selected=document.activeElement,
+			key,to,li;
+			if(this.contains(selected)){
+				key=selected.dataset.key||0;
+				while(selected.tagName!=="LI"){
+					selected=selected.parentNode;
+				}
+				delete this.querySelector("[data-active]").dataset.active;
+			}
+			else{
+				selected=this.querySelector("[data-active]")||this.querySelector("li:first-of-type");
+				key=selected.dataset.active||selected.dataset.key||0;
+			}
+			switch(input){
+				case "ArrowUp":
+				case "w":
+					li=selected;
+					do{
+						if(!li)return;
+						li=li.previousSibling;
+					}while(isValid(li));
 					if(!li)return;
-					li=li.previousSibling;
-				}while(isValid(li));
-				if(!li)return;
-				to=li.querySelector(`[data-key="${key}"]`);
-				break;
-			case "ArrowDown":
-			case "S":
-				li=selected;
-				do{
+					to=li.querySelector(`[data-key="${key}"]`);
+					break;
+				case "ArrowDown":
+				case "s":
+					li=selected;
+					do{
+						if(!li)return;
+						li=li.nextSibling;
+					}while(isValid(li));
 					if(!li)return;
-					li=li.nextSibling;
-				}while(isValid(li));
-				if(!li)return;
-				to=li.querySelector(`[data-key="${key}"]`);
-				break;
-			case "ArrowRight":
-			case "D":
-				key=(key+1)%3;
-				to=selected.querySelector(`[data-key="${key}"]`);
-				break;
-			case "ArrowLeft":
-			case "A":
-				key=(key-1)%3;
-				to=selected.querySelector(`[data-key="${key}"]`);
-				break;
-		}
-		if(to){
-			if(li)delete selected.dataset.active;
-			(li||selected).dataset.active=key;
-			to.focus();
+					to=li.querySelector(`[data-key="${key}"]`);
+					break;
+				case "ArrowRight":
+				case "d":
+					key=(key+1)%3;
+					to=selected.querySelector(`[data-key="${key}"]`);
+					break;
+				case "ArrowLeft":
+				case "a":
+					key=(key+2)%3;//(v-1)%m==(v-1+m)%m (For positive values)
+					to=selected.querySelector(`[data-key="${key}"]`);
+			}
+			if(to){
+				if(li)delete selected.dataset.active;
+				(li||selected).dataset.active=key;
+				to.focus();
+			}
 		}
 		function isValid(li){
 			return!(li instanceof HTMLElement)||li.style.display==="none"||(li.classList.contains("hideMod")&&/^\s*null/.test(document.getElementById("activeStyle2").innerHTML));
 		}
 	}
-});
-
 });
