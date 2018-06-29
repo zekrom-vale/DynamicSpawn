8c67c92c9926769e0851239082d9d0c28f7da98f
zekrom-vale <sgraven66@gmail.com>
2018-06-29 16:52:31 -0500

    Tryed to use a web worker

--- DynamicSpawn/js/custom.js
+++ DynamicSpawn/js/custom.js
@@ -43,23 +43,6 @@ document.getElementById("speciesInput").addEventListener("keydown",function(even
 	}
 });
 });
-//Web Worker
-var customNPC=(function(){
-	var[li,btn,img,img2,div]=baseLi,
-	el=li.cloneNode();
-	el.classList.add("custom-species");
-	var b=btn.cloneNode(),
-	divM=div.cloneNode(1);
-		divM.firstChild.classList.add("addToAll");
-		divM.childNodes[1].classList.add("removeFromAll");
-	var imgM=img.cloneNode();
-		imgM.src="img/tab_other.png";
-	var imgM2=img2.cloneNode();
-		imgM2.src="img/tab_other_select.png";
-		b.append(imgM,imgM2);
-	return[el,b,imgM,imgM2,divM];
-}());
-//Web Worker
 function customSetUp(specie){
 	var[li,btn,img,img2,div]=customNPC,
 	el=li.cloneNode();
