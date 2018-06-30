f1f865b1a5246124a9fea8f9292c51845848838c
zekrom-vale <sgraven66@gmail.com>
2018-06-30 18:18:48 -0500

    Compressed and added strict

--- DynamicSpawn/webWorker/nav.js
+++ DynamicSpawn/webWorker/nav.js
@@ -1,33 +1,33 @@
-onmessage=function(e){
+"use strict";
+onmessage=e=>{
 	postMessage(keyAction(e.data));
 }
 
 function keyAction(key){
 	switch(key.toLowerCase()){
-		case "r":
-		case "scrolllock":
-			return ["RegExpS"];
-		case "delete":
-			return ["removeAll"];
-		case "-":
-		case "_":
-		case "backspace":
-			return ["removeVisible"];
-		case "=":
-		case "+":
-			return ["addVisible"];
-		case "e":
-			return ["iexport"];
-		case "i":
-		case "insert":
-			return ["imp"];
-		case " ":
-		//case "end":
-			return ["download"];
-//
-		case "f":
-			return ["speciesInput","focus"];
-		case "o":
-			return ["searchOp","focus"];
+		case"r":
+		case"scrolllock":
+			return["RegExpS"];
+		case"delete":
+			return["removeAll"];
+		case"-":
+		case"_":
+		case"backspace":
+			return["removeVisible"];
+		case"=":
+		case"+":
+			return["addVisible"];
+		case"e":
+			return["iexport"];
+		case"i":
+		case"insert":
+			return["imp"];
+		case" ":
+		//case"end":
+			return["download"];
+		case"f":
+			return["speciesInput","focus"];
+		case"o":
+			return["searchOp","focus"];
 	}
 }
--- DynamicSpawn/webWorker/search.js
+++ DynamicSpawn/webWorker/search.js
@@ -1,3 +1,4 @@
+"use strict";
 var loop;
 onmessage=e=>{
 	if(e.data.init){
