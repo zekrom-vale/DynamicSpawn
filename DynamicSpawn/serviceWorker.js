adf6df47a9c71d3614b4485a9d11f3245485f5f7
zekrom-vale <sgraven66@gmail.com>
2018-07-03 16:47:44 -0500

    Added external sites

--- DynamicSpawn/serviceWorker.js
+++ DynamicSpawn/serviceWorker.js
@@ -5,6 +5,10 @@ addEventListener('install',event=>{
 	event.waitUntil(
 		caches.open(v).then(cache=>
 			cache.addAll([
+					'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js',
+					'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js',
+					'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js',
+					'https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
 				'/',
 				'/css/',
 					'/css/animation.css',
