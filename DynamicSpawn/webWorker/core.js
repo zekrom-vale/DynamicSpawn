8b764addb00cd834550400356dbf20bb69a9e34c
zekrom-vale <sgraven66@gmail.com>
2018-07-04 15:01:52 -0500

    Moved to seperate functions

--- DynamicSpawn/webWorker/core.js
+++ DynamicSpawn/webWorker/core.js
@@ -1,31 +1,58 @@
 "use strict";
 //requires CSP of `script-src 'unsafe-eval';` 
 if(self.Worker){
-var runWorker=(function(){
+var worker=(function(){
 	const worker=new Worker("webWorker/worker.js"),
 	end=new CustomEvent("end");
 	var response,
 	queue=[];//pseudo-queue
-	return(func,that,args={},save,Run=!save)=>{
-		const _q=queue.length;
-		if(_q>0){
-			queue[_q]={run:run.bind(_q,func,that,args,save,Run),event:document.createElement("i")};
-			return new Promise(r=>{
-				queue[_q].event.addEventListener("end",event=>{
-					r(response);
-					queue[_q]=undefined;
-				},{once:true});
-			});
-		}else{
-			queue[0]=undefined;
-			return new Promise(r=>{
-				worker.postMessage([func,that,args,save,Run]);
-				worker.onerror=worker.onmessage=e=>{
-					if(queue.length>1)queue[1].run();
-					else queue=[];
-					r(e.data||e);
-				}
-			});
+	return{
+		eval:(func,that,args={})=>{
+			const _q=queue.length;
+			if(_q>0){
+				queue[_q]={run:run.bind(_q,func,that,args,undefined,true),event:document.createElement("i")};
+				return new Promise(r=>{
+					queue[_q].event.addEventListener("end",event=>{
+						r(response);
+						queue[_q]=undefined;
+					},{once:true});
+				});
+			}else{
+				queue[0]=undefined;
+				return new Promise(r=>{
+					worker.postMessage([func,that,args,undefined,true]);
+					worker.onerror=worker.onmessage=e=>{
+						if(queue.length>1)queue[1].run();
+						else queue=[];
+						r(e.data||e);
+					}
+				});
+			}
+		},
+		save:(name,func,args=[])=>{
+			worker.postMessage([func,undefined,args,name,false]);
+		},
+		call:(func,that,args=[])=>{
+			const _q=queue.length;
+			if(_q>0){
+				queue[_q]={run:run.bind(_q,func,that,args,undefined,true),event:document.createElement("i")};
+				return new Promise(r=>{
+					queue[_q].event.addEventListener("end",event=>{
+						r(response);
+						queue[_q]=undefined;
+					},{once:true});
+				});
+			}else{
+				queue[0]=undefined;
+				return new Promise(r=>{
+					worker.postMessage([func,that,args,undefined,true]);
+					worker.onerror=worker.onmessage=e=>{
+						if(queue.length>1)queue[1].run();
+						else queue=[];
+						r(e.data||e);
+					}
+				});
+			}
 		}
 	}
 	function run(func,that,args,save,Run){
--- DynamicSpawn/webWorker/worker.js
+++ DynamicSpawn/webWorker/worker.js
@@ -3,8 +3,8 @@ const funcs={
 	//Other functions to be called
 };
 onmessage=e=>{
-	var func=funcs[e.data[0]]?funcs[e.data[0]]:new Function(...Object.keys(e.data[2]),e.data[0]);
-	if(e.data[4])postMessage(func.apply(e.data[1],Object.values(e.data[2])));
+	var func=funcs[e.data[0]]?funcs[e.data[0]]:new Function(...Array.isArray(e.data[2])?e.data[2]:Object.keys(e.data[2]),e.data[0]);
+	if(e.data[4])postMessage(func.apply(e.data[1],Array.isArray(e.data[2])?e.data[2]:Object.values(e.data[2])));
 	else postMessage("saved");
 	if(e.data[3])funcs[e.data[3]]=func;
 }
