5ea14c2336d3d38cce6fed0769ae16729757f8db
zekrom-vale <sgraven66@gmail.com>
2018-07-04 14:26:33 -0500

    Now can save functions and run them localy

--- DynamicSpawn/webWorker/core.js
+++ DynamicSpawn/webWorker/core.js
@@ -2,14 +2,14 @@
 //requires CSP of `script-src 'unsafe-eval';` 
 if(self.Worker){
 var runWorker=(function(){
-	var worker=new Worker("webWorker/worker.js"),
-	queue=[],//pseudo-queue
-	response,
+	const worker=new Worker("webWorker/worker.js"),
 	end=new CustomEvent("end");
-	return(func,that,args)=>{//{a:1,b:2}
-		var _q=queue.length;
+	var response,
+	queue=[];//pseudo-queue
+	return(func,that,args={},save,Run=!save)=>{
+		const _q=queue.length;
 		if(_q>0){
-			queue[_q]={run:run.bind(_q,func,that,args),event:document.createElement("i")};
+			queue[_q]={run:run.bind(_q,func,that,args,save,Run),event:document.createElement("i")};
 			return new Promise(r=>{
 				queue[_q].event.addEventListener("end",event=>{
 					r(response);
@@ -19,7 +19,7 @@ var runWorker=(function(){
 		}else{
 			queue[0]=undefined;
 			return new Promise(r=>{
-				worker.postMessage([func,that,args]);
+				worker.postMessage([func,that,args,save,Run]);
 				worker.onerror=worker.onmessage=e=>{
 					if(queue.length>1)queue[1].run();
 					else queue=[];
@@ -28,8 +28,8 @@ var runWorker=(function(){
 			});
 		}
 	}
-	function run(func,that,args){
-		worker.postMessage([func,that,args]);
+	function run(func,that,args,save,Run){
+		worker.postMessage([func,that,args,save,Run]);
 		worker.onerror=worker.onmessage=e=>{
 			response=e.data||e;
 			queue[this].event.dispatchEvent(end);
@@ -41,9 +41,10 @@ var runWorker=(function(){
 }
 else{
 	console.error("Web Worker not supported");
-	var runWorker=function(func){
+	var runWorker=function(func,that,args={}){
 		return new Promise(r=>{
-			r(func());
+			r(new Function(...Object.keys(args),func).apply(that,Object.values(args)));
 		});
 	}
 }
+//await runWorker("for(let i=0;i<1e+10;i++)continue;return 'done'");
--- DynamicSpawn/webWorker/worker.js
+++ DynamicSpawn/webWorker/worker.js
@@ -1,4 +1,10 @@
+const funcs={
+	//loop:function(){for(let i=0;i<1e+10;i++)continue;return 'done'}
+	//Other functions to be called
+};
 onmessage=e=>{
-	var func=new Function(...Object.keys(e.data[2]||{}),e.data[0]);
-	postMessage(func.apply(e.data[1],Object.values(e.data[2]||{})));
+	var func=funcs[e.data[0]]?funcs[e.data[0]]:new Function(...Object.keys(e.data[2]),e.data[0]);
+	if(e.data[4])postMessage(func.apply(e.data[1],Object.values(e.data[2])));
+	else postMessage("saved");
+	if(e.data[3])funcs[e.data[3]]=func;
 }
