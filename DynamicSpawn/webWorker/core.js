74e7bf3102dd098633f333f833fd35047909b8f9
zekrom-vale <sgraven66@gmail.com>
2018-07-05 13:36:14 -0500

    Removed redundency

--- DynamicSpawn/webWorker/core.js
+++ DynamicSpawn/webWorker/core.js
@@ -1,5 +1,5 @@
 "use strict";
-//requires CSP of `script-src 'unsafe-eval';` 
+//requires CSP of `script-src 'unsafe-eval';` to save functions
 if(self.Worker){
 var worker=(function(){
 	const worker=new Worker("webWorker/worker.js"),
@@ -7,10 +7,10 @@ var worker=(function(){
 	var response,
 	queue=[];//pseudo-queue
 	return{
-		eval:(func,that,args={})=>{
+		call:(func,that,args={})=>{
 			const _q=queue.length;
 			if(_q>0){
-				queue[_q]={run:run.bind(_q,func,that,args,undefined,true),event:document.createElement("i")};
+				queue[_q]={run:run.bind(_q,func,that,args),event:document.createElement("i")};
 				return new Promise(r=>{
 					queue[_q].event.addEventListener("end",event=>{
 						r(response);
@@ -20,7 +20,7 @@ var worker=(function(){
 			}else{
 				queue[0]=undefined;
 				return new Promise(r=>{
-					worker.postMessage([func,that,args,undefined,true]);
+					worker.postMessage([func,that,args]);
 					worker.onerror=worker.onmessage=e=>{
 						if(queue.length>1)queue[1].run();
 						else queue=[];
@@ -30,33 +30,11 @@ var worker=(function(){
 			}
 		},
 		save:(name,func,args=[])=>{
-			worker.postMessage([func,undefined,args,name,false]);
-		},
-		call:(func,that,args=[])=>{
-			const _q=queue.length;
-			if(_q>0){
-				queue[_q]={run:run.bind(_q,func,that,args,undefined,true),event:document.createElement("i")};
-				return new Promise(r=>{
-					queue[_q].event.addEventListener("end",event=>{
-						r(response);
-						queue[_q]=undefined;
-					},{once:true});
-				});
-			}else{
-				queue[0]=undefined;
-				return new Promise(r=>{
-					worker.postMessage([func,that,args,undefined,true]);
-					worker.onerror=worker.onmessage=e=>{
-						if(queue.length>1)queue[1].run();
-						else queue=[];
-						r(e.data||e);
-					}
-				});
-			}
+			worker.postMessage([func,undefined,args,name]);
 		}
 	}
-	function run(func,that,args,save,Run){
-		worker.postMessage([func,that,args,save,Run]);
+	function run(func,that,args){
+		worker.postMessage([func,that,args]);
 		worker.onerror=worker.onmessage=e=>{
 			response=e.data||e;
 			queue[this].event.dispatchEvent(end);
--- DynamicSpawn/webWorker/worker.js
+++ DynamicSpawn/webWorker/worker.js
@@ -1,10 +1,13 @@
 const funcs={
-	//loop:function(){for(let i=0;i<1e+10;i++)continue;return 'done'}
+	synchronousWait:(t=1e+10)=>{for(let i=0;i<t;i++)continue;return 'done'},
+	add:function(){return[...arguments].reduce((a,v)=>a+v)},
+	subtract:function(){return[...arguments].reduce((a,v)=>a-v)},
+	multiply:function(){return[...arguments].reduce((a,v)=>a*v)},
+	divide:function(){return[...arguments].reduce((a,v)=>a/v)}
 	//Other functions to be called
 };
 onmessage=e=>{
 	var func=funcs[e.data[0]]?funcs[e.data[0]]:new Function(...Array.isArray(e.data[2])?e.data[2]:Object.keys(e.data[2]),e.data[0]);
-	if(e.data[4])postMessage(func.apply(e.data[1],Array.isArray(e.data[2])?e.data[2]:Object.values(e.data[2])));
-	else postMessage("saved");
 	if(e.data[3])funcs[e.data[3]]=func;
+	else postMessage(func.apply(e.data[1],Array.isArray(e.data[2])?e.data[2]:Object.values(e.data[2])));
 }
