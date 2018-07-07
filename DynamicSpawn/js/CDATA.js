27c5dad39017faac250a3229e062c1ff3bfea67e
zekrom-vale <sgraven66@gmail.com>
2018-07-07 13:59:42 -0500

    Added attrabute functionality

--- DynamicSpawn/js/CDATA.js
+++ DynamicSpawn/js/CDATA.js
@@ -1,10 +1,12 @@
 //Use async and defer
+"use strict";
 addEventListener("DOMContentLoaded",()=>{
 	const data=document.querySelectorAll('script[type="CDATA"]'),
 	_d=data.length;
 	for(let i=0;i<_d;){
 		let node=data[i++];
-		node.parentNode.innerText=node.innerHTML;
+		if(node.dataset.attr)node.parentNode.setAttribute(node.dataset.attr,node.innerHTML);
+		else node.parentNode.innerText=node.innerHTML;
 		node.remove();
 	}
 },{once:true});
